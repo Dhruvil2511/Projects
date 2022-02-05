9a1300db8de322a91bef0924c7cd5a3d748c9178
Dhruvil Hitesh Prajapati <91490304+Dhruvil2511@users.noreply.github.com>
2022-02-05 20:08:22 +0530

    Update digital_clock.c

--- digital_clock.c
+++ digital_clock.c
@@ -1,4 +1,7 @@
 // Digital clock with an alarm
+// I made a small project of digital clock with alarm concept with the help of for loops and windows.h library
+// 1) To slower down the execution happening in seconds loop Sleep() func is used
+// 2) To make an alarm sound Beep() func is used in a loop
 #include <windows.h>
 #include <stdio.h>
 #include <time.h>
